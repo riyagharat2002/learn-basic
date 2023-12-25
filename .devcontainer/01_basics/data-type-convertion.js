ad3c5f539f5a5608d990902c6f35515ea7c6d2a7
riyagharat2002 <141639726+riyagharat2002@users.noreply.github.com>
2023-12-25 09:06:52 +0000

    datatype conversion

---
+++ .devcontainer/01_basics/data-type-convertion.js
@@ -0,0 +1,10 @@
+let matchScore = "100";
+
+let currentScore = Number(typeof(matchScore));
+console.log(currentScore);
+
+// conversion to boolean and remaining in same way
+let Score = "100";
+
+let curScore = Boolean(typeof(Score));
+console.log(curScore);
